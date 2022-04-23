07f8b54e3029ea9babfd5f55f852370c41ccd4ec
xpleiov <Sunny.helset@outlook.com>
2022-04-23 23:10:52 +0000

    add event listeners for document loading and button

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrimentScore(){
+
+}
+
+function incorementWrongAnswer(){
+
+}
+
+function displayAdditioQuestion(){
+
+}
+
+function displauSubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
